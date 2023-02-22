f1ca7d913d472ca9684cd7e19e5ac4618fb33518
Paula Pallarés <paulap@reby.co>
2023-02-22 12:05:00 +0100

    api file with api calls

---
+++ src/utils/api.js
@@ -0,0 +1,31 @@
+const API_URL = "https://front-test-api.herokuapp.com/api";
+
+const errorHandler = (error) => console.log("Error", error);
+
+export const fetchProducts = async () => {
+  return await fetch(`${API_URL}/product`)
+    .then((response) => response.json())
+    .catch(errorHandler);
+};
+
+export const fetchProduct = async (productId) => {
+  return await fetch(`${API_URL}/product/${productId}`)
+    .then((response) => response.json())
+    .catch(errorHandler);
+};
+
+export const addProductToCart = async ({ id, colorCode, storageCode }) => {
+  return await fetch(`${API_URL}/cart`, {
+    method: "POST",
+    body: JSON.stringify({
+      id,
+      colorCode,
+      storageCode,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .catch(errorHandler);
+};
